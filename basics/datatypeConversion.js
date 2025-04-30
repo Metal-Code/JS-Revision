1c85c918efe9f91e1eb4b1a1ff009d97c53293b3
Ayush Kumar <metalbond69@gmail.com>
2025-04-30 14:26:16 +0000

    dataTypeConversion

--- basics/datatypeConversion.js
+++ basics/datatypeConversion.js
@@ -31,6 +31,58 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 69;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+
+// ************* Operations ****************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = "Ayush";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// If the first operand is a string, the entire expression is treated as string concatenation; 
+// if it's a number, JavaScript adds numbers until it encounters a string, then switches to concatenation.
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+// console.log(gameCounter);
+
+
+
+// comparison
+// console.log("2" > 1);
+// console.log("02" > 1);
+// first converts 2 into number
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+// In JavaScript, null is converted to 0 in relational comparisons like >= or <, but not in equality (==) checks. 
+// That's why null >= 0 is true, but null > 0 and null == 0 are false.
+console.log("----")
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+// In JavaScript, undefined == 0 is false because undefined only equals null with ==.
+// In comparisons, undefined becomes NaN, so all relational checks like >, <, or >= return false.
