42d8fa44fea90a7306ab6af8b2adeb6bbdd06020
Ayush Kumar <metalbond69@gmail.com>
2025-04-30 13:55:46 +0000

    Conversion of datatypes

---
+++ basics/datatypeConversion.js
@@ -0,0 +1,36 @@
+let score = 22
+
+let temp = "69";
+let temp1 = "21ayush";
+let temp2 = null;
+
+let choice = true;
+// console.log(typeof score);
+// //or
+// console.log(typeof(score));
+
+// console.log(typeof temp);
+
+
+let valueInNumber = Number(temp);
+let valueInNumber1 = Number(temp1);
+let valueInNumber2 = Number(temp2);
+let valueInNumber3 = Number(choice);
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1);
+// console.log(typeof valueInNumber2);
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 69;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
