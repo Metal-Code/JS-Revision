8fb55e29193f292ea26259dcedad43fefd2eb093
Ayush Kumar <metalbond69@gmail.com>
2025-05-02 18:55:20 +0000

    Functions and objects

---
+++ function/functionsWithObjects.js
@@ -0,0 +1,23 @@
+// rest operator (same like spread  operator even syntax but it depends upon use case and stuff to determine whether it is pread or rest operator)
+// function calcCartPrice(...num1)
+// {
+//     return num1;
+// }
+
+function calcCartPrice(val1, val2, ...num1)
+{
+    return num1;
+}
+
+// console.log(calcCartPrice(200,400,500,800,900));
+
+const user = {
+    name : "Ayush",
+    price : "1999"
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+}
+handleObject(user)
