52edf9393107ae823028c2f650bc7654654a64a2
Ayush Kumar <metalbond69@gmail.com>
2025-05-05 18:15:36 +0000

    Switch Case

---
+++ control/switch.js
@@ -0,0 +1,28 @@
+// switch (key) 
+// {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+switch (month) 
+{
+    case 1:
+        console.log("Month January");
+        break;
+    case 2:
+        console.log("Month Feb");
+        break;
+    case 3:
+        console.log("Month march");
+        break;
+    default:
+        console.log("invalid month");
+        break;
+}
+
+// break statement will skip other cases and will just check for the defualt case at last
