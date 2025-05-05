a7e665b17697a458ae0592049563401ab77e3e9d
Ayush Kumar <metalbond69@gmail.com>
2025-05-05 19:11:15 +0000

    While and Do-while loops

---
+++ loops/while.js
@@ -0,0 +1,22 @@
+let index = 0;
+while (index <=10) 
+{
+    // console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+let counter = 0
+let arr = ["red", "green", "yellow", "blue", "pink"]
+while (counter < arr.length) 
+{
+    // console.log(`Value at ${counter+1} is ${arr[counter]}`);
+    counter++;
+}
+
+
+let score = 11
+do 
+{
+    console.log(`Score is ${score}`);
+    score++
+} while (score<10);
